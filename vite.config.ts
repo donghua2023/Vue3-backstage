859a9740232db783cd613123f95d9cd571291f31
浅陌 <1687516725@qq.com>
2023-05-04 17:50:38 +0800

    perf: path路径别名配置@

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+//以下两行代码是配置
+import path from 'path'
+const resolve = (dir:string) => path.resolve(__dirname, dir)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,5 +18,13 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
-  ]
+  ],
+  resolve:{
+    //这里是别名的映射规则
+    alias:{
+      '@': resolve('src'),
+      '@components': resolve('src/components'),
+      '@utils': resolve('src/utils')
+    }
+  }
 })
