57c50ee0dabdf35dde0f6892a482d02fda2da20f
浅陌 <1687516725@qq.com>
2023-06-19 16:22:34 +0800

    feat: 角色管理搜索区域搭建

---
+++ src/views/main/system/role/config/search.config.ts
@@ -0,0 +1,15 @@
+export const searchConfig: any = [
+  {
+    type: 'input',
+    label: '角色名称',
+    prop: 'name',
+    placeholder: '请输入查询的角色名称'
+  },
+  {
+    type: 'input',
+    label: '权限介绍',
+    prop: 'leader',
+    placeholder: '请输入查询的权限介绍'
+  },
+  { type: 'date', label: '创建时间', prop: 'createAt' }
+]
--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>role</div>
+  <div>
+    <pageSearch
+      :searchData="searchConfig"
+      @queryClick="handleQueryClick"
+    ></pageSearch>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import pageSearch from '@/components/page-search/page-search.vue'
+import { searchConfig } from './config/search.config.ts'
+
+const handleQueryClick = () => {
+  console.log('父组件查询')
+}
+</script>
 
 <style scoped></style>
