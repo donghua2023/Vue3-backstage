e8654a223409f34c137f7b58d78f8c9427b80c4f
浅陌 <1687516725@qq.com>
2023-06-19 16:44:25 +0800

    feat：部门页面搜索区域搭建

---
+++ src/views/main/system/department/config/search.config.ts
@@ -0,0 +1,15 @@
+export const searchConfig: any = [
+  {
+    type: 'input',
+    label: '部门名称',
+    prop: 'name',
+    placeholder: '请输入查询的部门名称'
+  },
+  {
+    type: 'input',
+    label: '部门领导',
+    prop: 'leader',
+    placeholder: '请输入查询的部门领导'
+  },
+  { type: 'date', label: '创建时间', prop: 'createAt' }
+]
