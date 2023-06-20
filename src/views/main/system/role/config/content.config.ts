d623e6a0972d8724a1b2bd916a238b6458447ca9
浅陌 <1687516725@qq.com>
2023-06-20 16:10:53 +0800

    feat：角色页面表格搭建完成

---
+++ src/views/main/system/role/config/content.config.ts
@@ -0,0 +1,12 @@
+export const contentconfig = {
+  tableColumns: [
+    {type: 'selection', width: "55"},
+    {type: 'index', label: '序号', width: "120"},
+    {prop: 'name', label: '角色名称', width: "180", visition: true},
+    {prop: 'intro', label: '权限介绍', width: "180", visition: true},
+    {prop: 'createAt', label: '创建时间', visition: true},
+    {prop: 'updateAt', label: '更新时间', visition: true},
+    {type: 'handler', label: '操作', visition: true, isEdit: true, isDelete: true}
+  ],
+  isPagination: true
+}
