643172e715056e6d67cde0f41e1a1d102b34cd70
浅陌 <1687516725@qq.com>
2023-07-03 16:51:37 +0800

    feat: 部门页面管理查询功能搭建完成

---
+++ src/service/main/system/department/department.ts
@@ -0,0 +1,8 @@
+import dhRequest from "@/service";
+
+export function postDepartmentList(param:any) {
+  return dhRequest.post({
+    url: '/department/list',
+    data: param
+  })
+}
--- src/stores/main/system/department/department.ts
+++ src/stores/main/system/department/department.ts
@@ -1,12 +1,24 @@
 import { defineStore } from 'pinia'
+import { postDepartmentList } from '@/service/main/system/department/department'
 
 interface Istate {
-  isSearchHide: Boolean
+  isSearchHide: Boolean,
+  departTableDate: Array<any>,
+  totalCount: number
 }
 const useDepartmentStore = defineStore('department', {
   state: ():Istate=> ({
-    isSearchHide: false
-  })
+    isSearchHide: false,
+    departTableDate: [],
+    totalCount: 0
+  }),
+  actions: {
+    async postDepartmentListAction(param:any) {
+      const { data } = await postDepartmentList(param)
+      this.departTableDate = data.list
+      this.totalCount = data.totalCount
+    }
+  }
 })
 
 export default useDepartmentStore
